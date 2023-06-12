1a334cf6e8e053687ee286f3f11a8500edf1fbfe
Sebastian Martinez <isaacsoccer235@gmail.com>
2023-06-12 18:43:53 -0500

    new

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About({bio, github, linkedin}) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{bio}</p>
+      {bio ? <p>{bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <h3>Links</h3>
       <a href={github}>{github}</a>
